8851b14119bde621403ced745733f34b6a2d9788
Devin Brown <pivotal@wazee.den.pivotal.io>
2015-09-16 16:17:55 -0600

    banana;

---
+++ components/Comment.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+class Counter extends Component {
+  render() {
+    return (
+      <h1>Comments</h1>
+    );
+  }
+}
+
+Counter.propTypes = {
+
+};
+
+export default Counter;
---
+++ server.js
@@ -0,0 +1,19 @@
+const PORT = 8080;
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: {
+    colors: true
+  }
+}).listen(PORT, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:' + PORT);
+});
